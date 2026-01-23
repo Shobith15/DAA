a929b931f9113d7e03b27f6739526eeea30214c3
Shobith IK <70338444+Shobith15@users.noreply.github.com>
2026-01-23 10:40:53 +0530

    Update lab3.java

--- lab3/lab3.java
+++ lab3/lab3.java
@@ -1,1 +1,33 @@
+class Main {
+    public static void main(String[] args) {
+        int[] a = {5, 3, 4, 1, 2};
+        int n = a.length;
+        int i, j, temp;
 
+        System.out.print("Initial: ");
+        printArray(a);
+
+        for (i = 1; i < n; i++) {
+            temp = a[i];
+            j = i - 1;
+
+            while (j >= 0 && a[j] > temp) {
+                a[j + 1] = a[j];
+                j--;
+            }
+
+            a[j + 1] = temp;
+
+            System.out.print("Pass " + i + ": ");
+            printArray(a);
+        }
+    }
+
+    static void printArray(int[] a) {
+    int i;
+    for (i = 0; i < a.length; i++) {
+        System.out.print(a[i] + " ");
+    }
+    System.out.println();
+}
+}
